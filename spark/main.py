107f047c833ea90ec78fcdca62f0e5d574160f5e
Usama Hussain <uhussain@wisc.edu>
2020-06-14 19:23:36 +0000

    Get Product prices however possible - expected big output to s3

--- spark/main.py
+++ spark/main.py
@@ -4,17 +4,17 @@ from pyspark.sql import SQLContext,SparkSession, functions as F
 from pyspark.sql.types import *
 import re
 import boto3
-import nltk
+#import nltk
 from warcio.archiveiterator import ArchiveIterator
 from io import BytesIO
 from bs4 import BeautifulSoup
 from bs4.dammit import EncodingDetector
-from nltk.stem.wordnet import WordNetLemmatizer
-nltk.download('punkt')
-Lem = WordNetLemmatizer()
-nltk.download('wordnet')
-nltk.download('words')
-from nltk import tokenize
+#from nltk.stem.wordnet import WordNetLemmatizer
+#nltk.download('punkt')
+#Lem = WordNetLemmatizer()
+#nltk.download('wordnet')
+#nltk.download('words')
+#from nltk import tokenize
 
 def html_to_text(page):
     """Converts html page to text
@@ -31,6 +31,51 @@ def html_to_text(page):
         return soup.get_text(" ", strip=True)
     except:
         return ""
+def extract_product_data(html_content):
+
+    # Create an Extractor by reading from the YAML file
+    #e = Extractor.from_yaml_file('PokemonShop.yml')
+    soup = BeautifulSoup(html_content,"html.parser")
+    #print(soup.prettify())
+
+    title=""
+    if soup.select("#productTitle"):
+        title = soup.select("#productTitle")[0].get_text().strip()
+    elif soup.select("#btAsinTitle"):
+        title = soup.select("#btAsinTitle")[0].get_text().strip()
+    #product_info.append(title)
+    price=""
+    if soup.select("#priceblock_saleprice"):
+        price = soup.select("#priceblock_saleprice")[0].get_text()
+    elif soup.select("#priceblock_ourprice"):
+        price = soup.select("#priceblock_ourprice")[0].get_text()
+    else:
+        #text = html_to_text(html_content)
+        text = soup.get_text()
+        #Look for $ prices in text
+        #print(text)
+        word_pattern = re.compile('\$+', re.UNICODE)
+        iterator = word_pattern.finditer(text)
+        #list of tuples (start,end) identifying all the places in the text where $ sign appears
+        price_index=[]
+        for match in iterator:
+            price_index.append(match.span())
+        sentences = []
+        #grab substrings ranging from text[start-300:end+10] using the $index above and add them to sentences list
+        #print("price_index: ",price_index)
+        for idx_tuple in price_index:
+            if (idx_tuple[0]-300) < 0:
+                sentences.append(text[0:idx_tuple[1]+10])
+            else:
+                sentences.append(text[idx_tuple[0]-300:idx_tuple[1]+10])
+        #print(sentences)
+        for sen in sentences:
+            if sen.find("$")!=-1:
+                price = sen[sen.find("$"):]
+    rating=""
+    if soup.select("#acrCustomerReviewText"):
+            rating = (soup.select("#acrCustomerReviewText")[0].get_text().split()[0])
+    return title,price,rating
 
 def fetch_process_warc_records(rows):
     """Retrieves document from S3 Data Lake.  The argument is a row of warc_filename, warc_record_offset, warc_record_length.  
@@ -42,45 +87,82 @@ def fetch_process_warc_records(rows):
     """
     s3client = boto3.client('s3')
     for row in rows:
+        #row = rows[i]
         warc_path = row['warc_filename']
         offset = int(row['warc_record_offset'])
         length = int(row['warc_record_length'])
         rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
         response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
+        #print(response)
         record_stream = BytesIO(response["Body"].read())
+        #print(record_stream)
         for record in ArchiveIterator(record_stream):
             page = record.content_stream().read()
-            text = html_to_text(page)
-            #words = map(lambda w: w, word_pattern.findall(text))
-            #result = tokenize.sent_tokenize(text)
-            #sentences = map(lambda s: s,tokenize.sent_tokenize(text))
-            #final = list(filter(lambda x: ('$' in x),result))
-            iterator = word_pattern.finditer(text)
-            #list of tuples (start,end) identifying all the places in the text where $ sign appears
-            price_index=[]
-            for match in iterator:
-                price_index.append(match.span())
-            sentences = []
-            #grab substrings ranging from text[start-300:end+10] using the $index above and add them to sentences list
-            for idx_tuple in price_index:
-                if (idx_tuple[0]-300) < 0:
-                    sentences.append(text[0:idx_tuple[1]+10])
-                else:
-                    sentences.append(text[idx_tuple[0]-300:idx_tuple[1]+10])
-            for sen in sentences:
-                title="None"
-                price="None"
-                if sen.find("$")!=-1:
-                    price = sen[sen.find("$"):]
-                    if (sen.find("Title")!=-1 and (sen.find("$")-100) > 0):
-                        title=sen[sen.find("Title")+6:sen.find("$")-100]
-                        yield title,price
-                    else:
-                        yield title,price
-                else:
-                    yield title,price
-            #for word in words:
-            #    yield word,1   #create key as word and index as 1
+            #print(type(page))
+            html_response=""
+            warc_input = page.decode("utf-8","ignore")
+            #print(warc_input)
+            try:
+                warc, header, html_response = warc_input.split('\r\n', 2)
+            except:
+                pass
+            #text = html_to_text(page)
+            #text = udf_text(page)
+            #print(text)
+            #print("WARC: ",warc)
+            #print("HEADER: ",header)
+            title,price,rating = extract_product_data(html_response)
+            yield title,price,rating
+
+#def fetch_process_warc_records(rows):
+#    """Retrieves document from S3 Data Lake.  The argument is a row of warc_filename, warc_record_offset, warc_record_length.  
+#       The html to be retrieved are pulled from the file using the offset and length.  
+#    Args:
+#        rows: list[string, int, int]
+#    Returns:
+#        fetch_process_warc_records
+#    """
+#    s3client = boto3.client('s3')
+#    for row in rows:
+#        warc_path = row['warc_filename']
+#        offset = int(row['warc_record_offset'])
+#        length = int(row['warc_record_length'])
+#        rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
+#        response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
+#        record_stream = BytesIO(response["Body"].read())
+#        for record in ArchiveIterator(record_stream):
+#            page = record.content_stream().read()
+#            text = html_to_text(page)
+#            #words = map(lambda w: w, word_pattern.findall(text))
+#            #result = tokenize.sent_tokenize(text)
+#            #sentences = map(lambda s: s,tokenize.sent_tokenize(text))
+#            #final = list(filter(lambda x: ('$' in x),result))
+#            iterator = word_pattern.finditer(text)
+#            #list of tuples (start,end) identifying all the places in the text where $ sign appears
+#            price_index=[]
+#            for match in iterator:
+#                price_index.append(match.span())
+#            sentences = []
+#            #grab substrings ranging from text[start-300:end+10] using the $index above and add them to sentences list
+#            for idx_tuple in price_index:
+#                if (idx_tuple[0]-300) < 0:
+#                    sentences.append(text[0:idx_tuple[1]+10])
+#                else:
+#                    sentences.append(text[idx_tuple[0]-300:idx_tuple[1]+10])
+#            for sen in sentences:
+#                title="None"
+#                price="None"
+#                if sen.find("$")!=-1:
+#                    price = sen[sen.find("$"):]
+#                    if (sen.find("Title")!=-1 and (sen.find("$")-100) > 0):
+#                        title=sen[sen.find("Title")+6:sen.find("$")-100]
+#                        yield title,price
+#                    else:
+#                        yield title,price
+#                else:
+#                    yield title,price
+#            #for word in words:
+#            #    yield word,1   #create key as word and index as 1
 
 
 session = SparkSession.builder.getOrCreate()  #Create Spark Session
@@ -89,8 +171,8 @@ session = SparkSession.builder.getOrCreate()  #Create Spark Session
 #sqldf = session.read.format("csv").option("header", True).option("inferSchema", True).load("s3://athena-east-2-usama/Unsaved/2020/06/09/112be0b3-7589-4b70-a240-10d781c28b60.csv")
 
 #Read parquet from athena query output
-sqldf = session.read.format("parquet").option("header", True).option("inferSchema", True).load('s3://athena-east-2-usama/Wal_query_2020_10_v2/*')
-
+sqldf = session.read.format("parquet").option("header", True).load('s3://athena-east-2-usama/Amazon_lap_2020_10/*')
+#sqldf = sqldf.repartition(12)
 #Read locally
 #sqldf = session.read.format("csv").option("header", True).option("inferSchema", True).load("walmart.csv")
 
@@ -98,7 +180,7 @@ sqldf = session.read.format("parquet").option("header", True).option("inferSchem
 warc_recs = sqldf.select("warc_filename", "warc_record_offset", "warc_record_length").rdd
 
 #Look for $ prices in text
-word_pattern = re.compile('\$+', re.UNICODE)
+#word_pattern = re.compile('\$+', re.UNICODE)
 
 #convert to unicode
 #word_pattern = re.compile('\w+', re.UNICODE)
@@ -112,7 +194,9 @@ word_pattern = re.compile('\$+', re.UNICODE)
 
 #filter on all products that have title None
 ## Filter out duplicates with distinct()
-products= warc_recs.mapPartitions(fetch_process_warc_records).filter((lambda a: "None" not in a[0])).distinct()
+products= warc_recs.mapPartitions(fetch_process_warc_records)
+#.filter((lambda a: "None" not in a[0]))
+#.distinct()
 sqlContext = SQLContext(session)
 
 #word_list= list(word_counts.take(1000))
@@ -129,10 +213,10 @@ sqlContext = SQLContext(session)
 #Product_Price = list(products)
 #print(Product_Price)                                  
 
-schemaProduct = sqlContext.createDataFrame(products,['Product','Price'])
+schemaProduct = sqlContext.createDataFrame(products,['Product','Price','Rating'])
 #print("schema: ",schemaWordCounts) 
 #=> schema:  DataFrame[_1: string, _2: bigint]
 
 #Write to parquet format
-schemaProduct.write.parquet("s3://athena-east-2-usama/Walmart_Products_2020_10/")
+schemaProduct.write.parquet("s3://athena-east-2-usama/Amazon_Products_2020_10/")
 #print(word_list)
