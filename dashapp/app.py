ac700271fb7666cf43dfec4865287d1cc9324e8b
Usama Hussain <uhussain@wisc.edu>
2020-06-19 20:06:45 +0000

    getting ready to commit to master

--- dashapp/app.py
+++ dashapp/app.py
@@ -10,19 +10,19 @@ import plotly.graph_objs as go
 app = dash.Dash()
 
 # Step 2. Import the dataset
-filepath = 'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv'
+filepath = 'datasets/laptop_trends_2017-2020.csv'
 st = pd.read_csv(filepath)
 
 # dropdown options
-features = st.columns[1:-1]
+features = st.columns[2:-1]
 opts = [{'label' : i, 'value' : i} for i in features]
 
 # range slider options
-st['Year'] = pd.to_datetime(st.Year)
+#st['Year'] = pd.to_datetime(st.Year)
 #dates = ['2015-02-17', '2015-05-17', '2015-08-17', '2015-11-17',
 #         '2016-02-17', '2016-05-17', '2016-08-17', '2016-11-17', '2017-02-17']
 
-dates = ['2017','2018','2019','2020']
+dates = [2017,2018,2019,2020]
 
 # Step 3. Create a plotly figure
 trace_1 = go.Scatter(x = st.Year, y = st['Low-Level'],
@@ -30,7 +30,11 @@ trace_1 = go.Scatter(x = st.Year, y = st['Low-Level'],
                     line = dict(width = 2,
                                 color = 'rgb(229, 151, 50)'))
 layout = go.Layout(title = 'Tracking Laptop Prices',
-                   hovermode = 'closest')
+                    xaxis={'title': 'Year'},
+                    yaxis={'title': 'Avg. Online Price'},
+                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                    legend={'x': 0, 'y': 1},
+                    hovermode = 'closest')
 fig = go.Figure(data = [trace_1], layout = layout)
 
 
@@ -61,7 +65,7 @@ app.layout = html.Div([
                                     marks = {i : dates[i] for i in range(0, 4)},
                                     min = 0,
                                     max = 3,
-                                    value = [1, 2])
+                                    value = [0, 3])
                         ], style = {'width' : '80%',
                                     'fontSize' : '20px',
                                     'padding-left' : '100px',
@@ -75,7 +79,7 @@ app.layout = html.Div([
              Input('slider', 'value')])
 def update_figure(input1, input2):
     # filtering the data
-    st2 = st[(st.Year > dates[input2[0]]) & (st.Year < dates[input2[1]])]
+    st2 = st[(st.Year >= dates[input2[0]]) & (st.Year <= dates[input2[1]])]
     # updating the plot
     trace_1 = go.Scatter(x = st2.Year, y = st2['Low-Level'],
                         name = 'Affordable',
--- spark/main.py
+++ spark/main.py
@@ -32,56 +32,56 @@ def html_to_text(page):
         return soup.get_text(" ", strip=True)
     except:
         return ""
-def extract_product_data(html_content):
-
-    # Create an Extractor by reading from the YAML file
-    #e = Extractor.from_yaml_file('PokemonShop.yml')
-    soup = BeautifulSoup(html_content,"html.parser")
-    #print(soup.prettify())
-
-    title=""
-    #Amazon product titles grabbed using html classes
-    if soup.select("#productTitle"):
-        title = soup.select("#productTitle")[0].get_text().strip()
-    elif soup.select("#btAsinTitle"):
-        title = soup.select("#btAsinTitle")[0].get_text().strip()
-    #product_info.append(title)
-    price=""
-    if soup.select("#priceblock_saleprice"):
-        price = soup.select("#priceblock_saleprice")[0].get_text()
-    elif soup.select("#priceblock_ourprice"):
-        price = soup.select("#priceblock_ourprice")[0].get_text()
-    else:
-        #text = html_to_text(html_content)
-        text = soup.get_text()
-        #Look for $ prices in text
-        #print(text)
-        word_pattern = re.compile('\$+', re.UNICODE)
-        iterator = word_pattern.finditer(text)
-        #list of tuples (start,end) identifying all the places in the text where $ sign appears
-        price_index=[]
-        for match in iterator:
-            price_index.append(match.span())
-        sentences = []
-        #grab substrings ranging from text[start-300:end+10] using the $index above and add them to sentences list
-        #print("price_index: ",price_index)
-        for idx_tuple in price_index:
-            if (idx_tuple[0]-300) < 0:
-                sentences.append(text[0:idx_tuple[1]+10])
-            else:
-                sentences.append(text[idx_tuple[0]-300:idx_tuple[1]+10])
-        #print(sentences)
-        for sen in sentences:
-            if sen.find("$")!=-1:
-                price = sen[sen.find("$"):]
-                #Walmart product titles grabbed from text
-                if title=="":
-                    if (sen.find("Title")!=-1 and (sen.find("$")-100) > 0):
-                        title=sen[sen.find("Title")+6:sen.find("$")-100]
-    rating=""
-    if soup.select("#acrCustomerReviewText"):
-            rating = (soup.select("#acrCustomerReviewText")[0].get_text().split()[0])
-    return title,price,rating
+#def extract_product_data(html_content):
+#
+#    # Create an Extractor by reading from the YAML file
+#    #e = Extractor.from_yaml_file('PokemonShop.yml')
+#    soup = BeautifulSoup(html_content,"html.parser")
+#    #print(soup.prettify())
+#
+#    title=""
+#    #Amazon product titles grabbed using html classes
+#    if soup.select("#productTitle"):
+#        title = soup.select("#productTitle")[0].get_text().strip()
+#    elif soup.select("#btAsinTitle"):
+#        title = soup.select("#btAsinTitle")[0].get_text().strip()
+#    #product_info.append(title)
+#    price=""
+#    if soup.select("#priceblock_saleprice"):
+#        price = soup.select("#priceblock_saleprice")[0].get_text()
+#    elif soup.select("#priceblock_ourprice"):
+#        price = soup.select("#priceblock_ourprice")[0].get_text()
+#    else:
+#        #text = html_to_text(html_content)
+#        text = soup.get_text()
+#        #Look for $ prices in text
+#        #print(text)
+#        word_pattern = re.compile('\$+', re.UNICODE)
+#        iterator = word_pattern.finditer(text)
+#        #list of tuples (start,end) identifying all the places in the text where $ sign appears
+#        price_index=[]
+#        for match in iterator:
+#            price_index.append(match.span())
+#        sentences = []
+#        #grab substrings ranging from text[start-300:end+10] using the $index above and add them to sentences list
+#        #print("price_index: ",price_index)
+#        for idx_tuple in price_index:
+#            if (idx_tuple[0]-300) < 0:
+#                sentences.append(text[0:idx_tuple[1]+10])
+#            else:
+#                sentences.append(text[idx_tuple[0]-300:idx_tuple[1]+10])
+#        #print(sentences)
+#        for sen in sentences:
+#            if sen.find("$")!=-1:
+#                price = sen[sen.find("$"):]
+#                #Walmart product titles grabbed from text
+#                if title=="":
+#                    if (sen.find("Title")!=-1 and (sen.find("$")-100) > 0):
+#                        title=sen[sen.find("Title")+6:sen.find("$")-100]
+#    #rating=""
+#    #if soup.select("#acrCustomerReviewText"):
+#    #        rating = (soup.select("#acrCustomerReviewText")[0].get_text().split()[0])
+#    return title,price
 
 def fetch_process_warc_records(rows):
     """Retrieves document from S3 Data Lake.  The argument is a row of warc_filename, warc_record_offset, warc_record_length.  
@@ -93,32 +93,83 @@ def fetch_process_warc_records(rows):
     """
     s3client = boto3.client('s3')
     for row in rows:
-        #row = rows[i]
         warc_path = row['warc_filename']
         offset = int(row['warc_record_offset'])
         length = int(row['warc_record_length'])
         rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
         response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
-        #print(response)
         record_stream = BytesIO(response["Body"].read())
-        #print(record_stream)
         for record in ArchiveIterator(record_stream):
             page = record.content_stream().read()
-            #print(type(page))
-            html_response=""
-            warc_input = page.decode("utf-8","ignore")
-            #print(warc_input)
-            try:
-                warc, header, html_response = warc_input.split('\r\n', 2)
-            except:
-                pass
-            #text = html_to_text(page)
-            #text = udf_text(page)
-            #print(text)
-            #print("WARC: ",warc)
-            #print("HEADER: ",header)
-            title,price,rating = extract_product_data(html_response)
-            yield title,price,rating
+            text = html_to_text(page)
+            #words = map(lambda w: w, word_pattern.findall(text))
+            #result = tokenize.sent_tokenize(text)
+            #sentences = map(lambda s: s,tokenize.sent_tokenize(text))
+            #final = list(filter(lambda x: ('$' in x),result))
+            word_pattern = re.compile('\$+', re.UNICODE)
+            iterator = word_pattern.finditer(text)
+            #list of tuples (start,end) identifying all the places in the text where $ sign appears
+            price_index=[]
+            for match in iterator:
+                price_index.append(match.span())
+            sentences = []
+            #grab substrings ranging from text[start-300:end+10] using the $index above and add them to sentences list
+            for idx_tuple in price_index:
+                if (idx_tuple[0]-300) < 0:
+                    sentences.append(text[0:idx_tuple[1]+10])
+                else:
+                    sentences.append(text[idx_tuple[0]-300:idx_tuple[1]+10])
+            for sen in sentences:
+                title="None"
+                price="None"
+                if sen.find("$")!=-1:
+                    price = sen[sen.find("$"):]
+                    if (sen.find("Title")!=-1 and (sen.find("$")-100) > 0):
+                        title=sen[sen.find("Title")+6:sen.find("$")-100]
+                        yield title,price
+                    else:
+                        yield title,price
+                else:
+                    yield title,price
+            #for word in words:
+            #    yield word,1   #create key as word and index as 1
+#Function for Amazon
+#def fetch_process_warc_records(rows):
+#    """Retrieves document from S3 Data Lake.  The argument is a row of warc_filename, warc_record_offset, warc_record_length.  
+#       The html to be retrieved are pulled from the file using the offset and length.  
+#    Args:
+#        rows: list[string, int, int]
+#    Returns:
+#        fetch_process_warc_records
+#    """
+#    s3client = boto3.client('s3')
+#    for row in rows:
+#        #row = rows[i]
+#        warc_path = row['warc_filename']
+#        offset = int(row['warc_record_offset'])
+#        length = int(row['warc_record_length'])
+#        rangereq = 'bytes={}-{}'.format(offset, (offset+length-1))
+#        response = s3client.get_object(Bucket='commoncrawl', Key=warc_path, Range=rangereq)
+#        #print(response)
+#        record_stream = BytesIO(response["Body"].read())
+#        #print(record_stream)
+#        for record in ArchiveIterator(record_stream):
+#            page = record.content_stream().read()
+#            #print(type(page))
+#            html_response=""
+#            warc_input = page.decode("utf-8","ignore")
+#            #print(warc_input)
+#            try:
+#                warc, header, html_response = warc_input.split('\r\n', 2)
+#            except:
+#                pass
+#            #text = html_to_text(page)
+#            #text = udf_text(page)
+#            #print(text)
+#            #print("WARC: ",warc)
+#            #print("HEADER: ",header)
+#            title,price = extract_product_data(html_response)
+#            yield title,price
 
 args = getComLineArgs()
 conf = SparkConf().setAll([("spark.executor.memory", "10g"),
@@ -133,19 +184,19 @@ sqldf = session.read.format("parquet").option("header", True).load(input_path+'/
 #sqldf = sqldf.repartition(12)
 
 #Create rdd of the 1000 rows selected
-warc_recs = sqldf.select("warc_filename", "warc_record_offset", "warc_record_length").rdd
+warc_recs = sqldf.select("warc_filename", "warc_record_offset", "warc_record_length").rdd.repartition(40)
 
 print("NumOfPartitions: ",warc_recs.getNumPartitions())
 products= warc_recs.mapPartitions(fetch_process_warc_records)
 sqlContext = SQLContext(session)
 
-products.repartition(40)
-schemaProduct = sqlContext.createDataFrame(products,['Product','Price','Rating'])
+#schemaProduct = sqlContext.createDataFrame(products,['Product','Price','Rating'])
 
+schemaProduct = sqlContext.createDataFrame(products,['Product','Price'])
 #Write to parquet format
 #schemaProduct.write.parquet("s3://athena-east-2-usama/Amazon_Products_2018-47/")
 output_path = "s3://output-east-2-usama/%s/" % (args['output_path'])
-schemaProduct.write.csv(output_path)
+schemaProduct.write.parquet(output_path)
 #Write to csv format
 #schemaProduct.write.csv("s3://output-east-2-usama/Walmart_laptops_2019_09/")
 #print(word_list)
